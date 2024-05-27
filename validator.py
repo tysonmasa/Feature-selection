343d41ae632950e2135e279dd0e83bfe1a632d67
aguin005 <aguin005@ucr.edu>
2024-05-26 20:42:03 -0700

    Art - fix validator evaluate def

--- validator.py
+++ validator.py
@@ -2,15 +2,21 @@ import numpy as np
 from sklearn.base import BaseEstimator
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
 
 class Validator:
-    def init(self, classifier: BaseEstimator):
+    def __init__(self, classifier: BaseEstimator):
         self.classifier = classifier
 
     def evaluate(self, X: np.ndarray, y: np.ndarray, feature_subset: list) -> float:
         X_subset = X[:, feature_subset]
-        X_train, X_test, y_train, y_test = train_test_split(X_subset, y, test_size=0.3, random_state=42)
-        self.classifier.fit(X_train, y_train)
-        y_pred = self.classifier.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        return accuracy
+        accuracy = []
+        for i in range(len(X_subset)):
+            X_train = np.concatenate((X_subset[:i], X_subset[i+1:]))
+            y_train = np.concatenate((y[:i], y[i+1:]))
+            X_test, y_test = np.array([X_subset[i]]), np.array([y[i]])
+            self.classifier.fit(X_train, y_train)
+            y_pred = self.classifier.predict(X_test)
+            accuracy.append(accuracy_score(y_test, y_pred))
+        return np.mean(accuracy)
+    
